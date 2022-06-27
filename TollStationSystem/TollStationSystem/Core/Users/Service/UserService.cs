d2c21bcc2063f9a31390fc3833d8c7393202f961
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-06-27 12:00:45 +0200

    Created Packaging Structure

---
+++ TollStationSystem/TollStationSystem/Core/Users/Service/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollStationSystem.Core.Users.Service
+{
+    class UserService
+    {
+    }
+}

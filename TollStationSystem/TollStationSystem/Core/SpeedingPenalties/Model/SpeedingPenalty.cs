a24c8a8f33a86dad9356c94be1387f4c28e24a16
cuturic01 <miloscuturic8@gmail.com>
2022-06-27 15:47:34 +0200

    Implemented assigned models and repos
    Implemented model and repository classes for SpeedingPenalties, TollBooths and TollStations.

--- TollStationSystem/TollStationSystem/Core/SpeedingPenalties/Model/SpeedingPenalty.cs
+++ TollStationSystem/TollStationSystem/Core/SpeedingPenalties/Model/SpeedingPenalty.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace TollStationSystem.Core.SpeedingPenalties.Model
 {
-    class SpeedingPenalty
+    public class SpeedingPenalty
     {
+        int id;
+        int paymentId;
+        DateTime date;
+        float overdraft;
+
+        public SpeedingPenalty() { }
+
+        public SpeedingPenalty(int id, int paymentId, DateTime date, float overdraft)
+        {
+            this.id = id;
+            this.paymentId = paymentId;
+            this.date = date;
+            this.overdraft = overdraft;
+        }
+
+        [JsonPropertyName("id")]
+        public int Id { get => id; set => id = value; }
+
+        [JsonPropertyName("paymentId")]
+        public int PaymentId { get => paymentId; set => paymentId = value; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get => date; set => date = value; }
+
+        [JsonPropertyName("overdraft")]
+        public float Overdraft { get => overdraft; set => overdraft = value; }
+
+        public override string ToString()
+        {
+            return "SpeedingPenalty[id: " + id + ", paymentId: " + paymentId
+                + ", date: " + date.ToString("dd-MM-yyyy") + ", overdraft: " + overdraft + "]";
+        }
     }
 }
--- TollStationSystem/TollStationSystem/Core/TollBooths/Model/TollBooth.cs
+++ TollStationSystem/TollStationSystem/Core/TollBooths/Model/TollBooth.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace TollStationSystem.Core.TollBooths.Model
 {
-    class TollBooth
+    public class TollBooth
     {
+        int tollStationId;
+        int number;
+        TollBoothType tollBoothType;
+        bool malfunctioning;
+        List<int> devices;
+
+        public TollBooth()
+        {
+            devices = new List<int>();
+        }
+
+        public TollBooth(int tollStationId, int number, TollBoothType tollBoothType, bool malfunctioning,
+            List<int> devices)
+        {
+            this.tollStationId = tollStationId;
+            this.number = number;
+            this.tollBoothType = tollBoothType;
+            this.malfunctioning = malfunctioning;
+            this.devices = devices;
+        }
+
+        [JsonPropertyName("tollStationId")]
+        public int TollStationId { get => tollStationId; set => tollStationId = value; }
+
+        [JsonPropertyName("number")]
+        public int Number { get => number; set => number = value; }
+
+        [JsonPropertyName("tollBoothType")]
+        public TollBoothType TollBoothType { get => tollBoothType; set => tollBoothType = value; }
+
+        [JsonPropertyName("malfunctioning")]
+        public bool Malfunctioning { get => malfunctioning; set => malfunctioning = value; }
+
+        [JsonPropertyName("devices")]
+        public List<int> Devices { get => devices; set => devices = value; }
+
+        public override string ToString()
+        {
+            return "TollBooth[tollStationId: " + tollStationId + ", number: " + number
+                + ", tollBoothType: " + tollBoothType + ", malfunctioning: " + malfunctioning.ToString()
+                + ", devices:" + devices.Count + "]";
+        }
     }
 }

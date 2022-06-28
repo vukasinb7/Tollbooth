25a27aa843fbb850faf677382ed3494ddc423638
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-06-28 20:36:10 +0200

    Added Services

--- TollStationSystem/TollStationSystem/Core/DeploymentHistory/Service/DeploymentHistoryService.cs
+++ TollStationSystem/TollStationSystem/Core/DeploymentHistory/Service/DeploymentHistoryService.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TollStationSystem.Core.DeploymentHistory.Model;
+using TollStationSystem.Core.DeploymentHistory.Repository;
 
 namespace TollStationSystem.Core.DeploymentHistory.Service
 {
-    class DeploymentHistoryService
+    public class DeploymentHistoryService : IDeploymentHistoryService
     {
+        IDeploymentHistoryRepo deploymentHistoryRepo;
+
+        public DeploymentHistoryService(IDeploymentHistoryRepo deploymentHistoryRepo)
+        {
+            this.deploymentHistoryRepo = deploymentHistoryRepo;
+        }
+
+        public List<DeploymentHistoryRecord> DeploymentHistoryRecords
+        { get => deploymentHistoryRepo.DeploymentHistoryRecords; }
+
+        public void Add(DeploymentHistoryRecord deploymentHistoryRecord)
+        {
+            deploymentHistoryRepo.Add(deploymentHistoryRecord);
+        }
+
+        public List<DeploymentHistoryRecord> FilterByBossJmbg(string jmbg)
+        {
+            return deploymentHistoryRepo.FilterByBossJmbg(jmbg);
+        }
+
+        public List<DeploymentHistoryRecord> FilterByStationId(int id)
+        {
+            return deploymentHistoryRepo.FilterByStationId(id);
+        }
+
+        public void Load()
+        {
+            deploymentHistoryRepo.Load();
+        }
+
+        public void Serialize()
+        {
+            deploymentHistoryRepo.Serialize();
+        }
     }
 }
---
+++ TollStationSystem/TollStationSystem/GUI/Controllers/Locations/LocationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TollStationSystem.Core.Locations.Model;
+using TollStationSystem.Core.Locations.Service;
+
+namespace TollStationSystem.GUI.Controllers.Locations
+{
+    public class LocationController
+    {
+        ILocationService locationService;
+
+        public LocationController(ILocationService locationService)
+        {
+            this.locationService = locationService;
+        }
+
+        public List<Location> Locations { get => locationService.Locations; }
+
+        public void Add(Location location)
+        {
+            locationService.Add(location);
+        }
+
+        public Location FindByZip(string zip)
+        {
+            return locationService.FindByZip(zip);
+        }
+
+        public void Load()
+        {
+            locationService.Load();
+        }
+
+        public void Serialize()
+        {
+            locationService.Serialize();
+        }
+    }
+}

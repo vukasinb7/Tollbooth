21492f4b9d9cfd7103f9cf531d52585f4a9f34f0
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-06-28 21:11:40 +0200

    Added Manager GUI

---
+++ TollStationSystem/TollStationSystem/GUI/View/ManagerView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TollStationSystem.GUI.View
+{
+    /// <summary>
+    /// Interaction logic for ManagerView.xaml
+    /// </summary>
+    public partial class ManagerView : Window
+    {
+        public ManagerView()
+        {
+            InitializeComponent();
+        }
+    }
+}

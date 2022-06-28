4542343b328db624bbbb3cb01577be6d5faedf63
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2022-06-28 21:45:52 +0200

    Implemented GUI for boss

---
+++ TollStationSystem/TollStationSystem/GUI/View/BossView/BossWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TollStationSystem.GUI.View.BossView
+{
+    /// <summary>
+    /// Interaction logic for BossWindow.xaml
+    /// </summary>
+    public partial class BossWindow : Window
+    {
+        public BossWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

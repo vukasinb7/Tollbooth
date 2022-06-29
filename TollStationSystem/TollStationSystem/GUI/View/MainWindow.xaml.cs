328ad8baee543394cbd030e6c39a8c92786b59f5
cuturic01 <miloscuturic8@gmail.com>
2022-06-29 11:24:44 +0200

    Implemented login

--- TollStationSystem/TollStationSystem/GUI/View/MainWindow.xaml.cs
+++ TollStationSystem/TollStationSystem/GUI/View/MainWindow.xaml.cs
@@ -1,28 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using TollStationSystem.Core.Users.Model;
+using TollStationSystem.Database;
+using TollStationSystem.GUI.Controller.Users;
 
 namespace TollStationSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        ServiceBuilder serviceBuilder;
+        UserController userController;
+
         public MainWindow()
         {
+            serviceBuilder = new();
+            userController = new(serviceBuilder.UserService);
             InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTb.Text;
+            string password = passwordTb.Password;
+
+            User user = userController.Login(username, password);
+            if (user is null)
+            {
+                MessageBox.Show("Invalid username or password!");
+                passwordTb.Clear();
+            }
+            else if (user.UserType == UserType.ADMINISTRATOR)
+            {
+                MessageBox.Show("Logged in as admin");
+            }
+            else if (user.UserType == UserType.BOSS)
+            {
+                MessageBox.Show("Logged in as boss");
+            }
+            else if (user.UserType == UserType.CLERK)
+            {
+                MessageBox.Show("Logged in as clerk");
+            }
+            else if (user.UserType == UserType.MANAGER)
+            {
+                MessageBox.Show("Logged in as manager");
+            }
         }
     }
 }

2f4b90164b53a0f114d13330359a8e7adcdfdd01
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2022-06-29 10:52:16 +0200

    Implemented service builder and database

--- TollStationSystem/TollStationSystem/Database/TollBoothDatabase.cs
+++ TollStationSystem/TollStationSystem/Database/TollBoothDatabase.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TollStationSystem.Core.DeploymentHistory.Repository;
+using TollStationSystem.Core.Devices.Repository;
+using TollStationSystem.Core.Locations.Repository;
+using TollStationSystem.Core.Payments.Repository;
+using TollStationSystem.Core.PriceLists.Repository;
+using TollStationSystem.Core.Sections.Repository;
+using TollStationSystem.Core.SpeedingPenalties.Repository;
+using TollStationSystem.Core.TollBooths.Repository;
+using TollStationSystem.Core.TollStations.Repository;
+using TollStationSystem.Core.Users.Repository;
 
 namespace TollStationSystem
 {
@@ -18,8 +28,59 @@ namespace TollStationSystem
 namespace TollStationSystem.Database
 {
     
-    class TollBoothDatabase
+    public class TollBoothDatabase
     {
+        IDeploymentHistoryRepo deploymentHistoryRepo;
+        IDeviceRepo deviceRepo;
+        ILocationRepo locationRepo;
+        IPaymentRepo paymentRepo;
+        IPriceListRepo priceListRepo;
+        ISectionRepo sectionRepo;
+        ISpeedingPenaltyRepo speedingPenaltyRepo;
+        ITollBoothRepo tollBoothRepo;
+        ITollStationRepo tollStationRepo;
+        IBossRepo bossRepo;
+        ITagUserRepo tagUserRepo;
+        IUserRepo userRepo;
 
+        public TollBoothDatabase()
+        {
+            deploymentHistoryRepo = new DeploymentHistoryRepo();
+            deviceRepo = new DeviceRepo();
+            locationRepo = new LocationRepo();
+            paymentRepo = new PaymentRepo();
+            priceListRepo = new PriceListRepo();
+            sectionRepo = new SectionRepo();
+            speedingPenaltyRepo = new SpeedingPenaltyRepo();
+            tollBoothRepo = new TollBoothRepo();
+            tollStationRepo = new TollStationRepo();
+            bossRepo = new BossRepo();
+            tagUserRepo = new TagUserRepo();
+            userRepo = new UserRepo();
+        }
+
+        public IDeploymentHistoryRepo DeploymentHistoryRepo { get => deploymentHistoryRepo; }
+
+        public IDeviceRepo DeviceRepo { get => deviceRepo; set => deviceRepo = value; }
+
+        public ILocationRepo LocationRepo { get => locationRepo; }
+
+        public IPaymentRepo PaymentRepo { get => paymentRepo; }
+
+        public IPriceListRepo PriceListRepo { get => priceListRepo; }
+
+        public ISpeedingPenaltyRepo SpeedingPenaltyRepo { get => speedingPenaltyRepo; }
+
+        public ITollStationRepo TollStationRepo { get => tollStationRepo; }
+
+        public IBossRepo BossRepo { get => bossRepo; }
+
+        public ITagUserRepo TagUserRepo { get => tagUserRepo; }
+
+        public IUserRepo UserRepo { get => userRepo; }
+
+        public ISectionRepo SectionRepo { get => sectionRepo; }
+
+        public ITollBoothRepo TollBoothRepo { get => tollBoothRepo; }
     }
 }
